9860468d97b5f86f24dfb065a88316bff8a5e163
Walid Bellili <walid.bellili.seixas@gmail.com>
2023-04-24 20:58:59 +0200

    réorganisation

---
+++ src/components/Hero.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+
+
+
+const Hero = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      wpPage(id: { eq: "cG9zdDoyOA==" }) {
+        id
+        title
+        content
+        featuredImage {
+          node {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(width: 800, placeholder: BLURRED)
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  const { title, content, featuredImage } = data.wpPage;
+  return (
+    <div>       
+      <h1 className="container mx-auto">{title}</h1>
+      {featuredImage && (
+        <img
+          src={featuredImage.node.localFile.childImageSharp.gatsbyImageData.images.fallback.src}
+          alt="Featured Image"
+        />
+      )}
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,41 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+// import { graphql, useStaticQuery } from "gatsby";
+import Hero from "../components/Hero";
 
 const Index = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      wpPage(id: { eq: "cG9zdDoyOA==" }) {
-        id
-        title
-        content
-        featuredImage {
-          node {
-            localFile {
-              childImageSharp {
-                gatsbyImageData(width: 800, placeholder: BLURRED)
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
+//   const data = useStaticQuery(graphql`
+//     query {
+//       wpPage(id: { eq: "cG9zdDoyOA==" }) {
+//         id
+//         title
+//         content
+//         featuredImage {
+//           node {
+//             localFile {
+//               childImageSharp {
+//                 gatsbyImageData(width: 800, placeholder: BLURRED)
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
 
-  const { title, content, featuredImage } = data.wpPage;
+//   const { title, content, featuredImage } = data.wpPage;
 
   return (
-    <div>
-      <h1>{title}</h1>
+    <div>       
+    <Hero/>
+      {/* <h1>{title}</h1>
       <div dangerouslySetInnerHTML={{ __html: content }} />
       {featuredImage && (
         <img
           src={featuredImage.node.localFile.childImageSharp.gatsbyImageData.images.fallback.src}
           alt="Featured Image"
         />
-      )}
+      )} */}
+
     </div>
   );
 };
