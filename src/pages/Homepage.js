397f2f283ddadd96b16e5f66ba8aa6e42a9e1842
Walid Bellili <walid.bellili.seixas@gmail.com>
2023-04-26 22:26:52 +0200

    page contact et formulaire pop up pret reste à recuperer la data et l'envoyer

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,6 @@ import parse from 'html-react-parser';
 import Nav from '../components/NavBar';
 import Hero from '../components/Hero';
 import HomePosts from '../components/Posts/HomePosts';
-import ProductPosts from '../components/Posts/ProductPosts';
 
 const Homepage = () => {
   const query = useStaticQuery(graphql`
@@ -27,7 +26,6 @@ const Homepage = () => {
       <Nav />
       <Hero title={query.wpPage.title} img={srcValue} headline={headline} />
       <HomePosts />
-      <ProductPosts />
     </div>
   );
 };
