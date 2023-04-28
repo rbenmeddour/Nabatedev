d4f50193dd1a6e3e76127aa1a163db897be2a39a
Walid Bellili <walid.bellili.seixas@gmail.com>
2023-04-28 16:51:28 +0200

    rebasing test

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,27 @@
-const path = require("path")
+// const path = require("path")
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const pageTemplate = path.resolve("./src/template/page.js")
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const pageTemplate = path.resolve("./src/template/page.js")
 
-  const result = await graphql(`
-    {
-      allWpPage {
-        nodes {
-          id
-          slug
-        }
-      }
-    }
-  `)
+//   const result = await graphql(`
+//     {
+//       allWpPage {
+//         nodes {
+//           id
+//           slug
+//         }
+//       }
+//     }
+//   `)
 
-  result.data.allWpPage.nodes.forEach(node => {
-    createPage({
-      path: `/${node.slug}`,
-      component: pageTemplate,
-      context: {
-        id: node.id,
-      },
-    })
-  })
-}
+//   result.data.allWpPage.nodes.forEach(node => {
+//     createPage({
+//       path: `/${node.slug}`,
+//       component: pageTemplate,
+//       context: {
+//         id: node.id,
+//       },
+//     })
+//   })
+// }
