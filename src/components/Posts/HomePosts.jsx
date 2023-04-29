dfe167debbfb4def8119a8040504cc3208f6bd66
Omnivers <twersimor.stock@outlook.com>
2023-04-29 23:50:28 +0200

    Article switching imgs

--- src/components/Posts/HomePosts.jsx
+++ src/components/Posts/HomePosts.jsx
@@ -3,10 +3,10 @@ import { graphql, useStaticQuery } from "gatsby";
 import parse from "html-react-parser";
 import Article from "../Article";
 
-const HomePosts = ()=> {
-    const queryPosts = useStaticQuery(graphql`
+const HomePosts = () => {
+  const queryPosts = useStaticQuery(graphql`
     query {
-        allWpPost  {
+      allWpPost {
         edges {
           node {
             title
@@ -18,20 +18,41 @@ const HomePosts = ()=> {
             }
           }
         }
-        }
-    }`)
-    const filteredPosts = queryPosts.allWpPost.edges.filter(edge => edge.node.categories.nodes.some(node => node.id === "dGVybTo0Ng=="))
+      }
+    }
+  `);
 
-    return( 
-        <>
-            {filteredPosts.map((post, index) => {
-                const parsedContent = parse(post.node.content);
-                const postParagraph = parsedContent[3].props.children;
-                const postImg = parsedContent[1].props.children.props.src;
-                const postTitle = post.node.title;
-                return <Article key={index} image={postImg} paragraph={postParagraph} title={postTitle} />;
-            })}
-        </>
-    )
-}
+  const filteredPosts = queryPosts.allWpPost.edges.filter((edge) =>
+    edge.node.categories.nodes.some((node) => node.id === "dGVybTo0Ng==")
+  );
+
+  return (
+    <>
+      {filteredPosts.map((post, index) => {
+        const parsedContent = parse(post.node.content);
+        const postParagraph = parsedContent[3].props.children;
+        let postImg;
+        if (parsedContent[1].props.children.props?.src) {
+          postImg = parsedContent[1].props.children.props?.src;
+        } else {
+          postImg =
+            parsedContent[1].props.children[0].props.children[2].props[
+              "data-src"
+            ];
+        }
+        const postTitle = post.node.title;
+        const reverse = index % 2 === 0;
+        return (
+          <Article
+            key={index}
+            image={postImg}
+            paragraph={postParagraph}
+            title={postTitle}
+            reverse={reverse}
+          />
+        );
+      })}
+    </>
+  );
+};
 export default HomePosts;
