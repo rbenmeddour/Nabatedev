31d579218f12c4a0bbdf98ce4c5ae37aec447417
Walid Bellili <walid.bellili.seixas@gmail.com>
2023-04-29 23:00:31 +0200

    handled conflicts

--- src/components/Posts/HomePosts.jsx
+++ src/components/Posts/HomePosts.jsx
@@ -3,10 +3,10 @@ import { graphql, useStaticQuery } from "gatsby";
 import parse from "html-react-parser";
 import Article from "../Article";
 
-const HomePosts = () => {
-  const queryPosts = useStaticQuery(graphql`
+const HomePosts = ()=> {
+    const queryPosts = useStaticQuery(graphql`
     query {
-      allWpPost {
+        allWpPost  {
         edges {
           node {
             title
@@ -18,10 +18,8 @@ const HomePosts = () => {
             }
           }
         }
-<<<<<<< HEAD
         }
     }`)
-<<<<<<< HEAD
     const filteredPosts = queryPosts.allWpPost.edges.filter(edge => edge.node.categories.nodes.some(node => node.id === "dGVybTo0Ng=="))
 
     return( 
@@ -33,75 +31,7 @@ const HomePosts = () => {
                 const postTitle = post.node.title;
                 return <Article key={index} image={postImg} paragraph={postParagraph} title={postTitle} />;
             })}
-=======
-
-    const filteredPosts = queryPosts.allWpPost.edges.filter(edge => edge.node.categories.nodes.some(node => node.id === "dGVybTo2MA=="))
-    
-    return( 
-        <>
-          {filteredPosts.map((post, index) => {
-              const parsedContent = parse(post.node.content);
-              const postParagraph = parsedContent[3].props.children;
-              let postImg;
-              if(parsedContent[1].props.children.props?.src){
-                postImg = parsedContent[1].props.children.props?.src;
-              }else{
-                postImg = parsedContent[1].props.children[0].props.children[2].props['data-src'];
-              }
-              const postTitle = post.node.title;
-              const reverse = index % 2 === 0; // true for even index, false for odd index
-                return (
-                  <Article
-                    key={index}
-                    image={postImg}
-                    paragraph={postParagraph}
-                    title={postTitle}
-                    reverse={reverse}
-                  />
-                );
-              })}
->>>>>>> jad
         </>
     )
 }
 export default HomePosts;
-=======
-      }
-    }
-  `);
-
-  const filteredPosts = queryPosts.allWpPost.edges.filter((edge) =>
-    edge.node.categories.nodes.some((node) => node.id === "dGVybTo0Ng==")
-  );
-
-  return (
-    <>
-      {filteredPosts.map((post, index) => {
-        const parsedContent = parse(post.node.content);
-        const postParagraph = parsedContent[3].props.children;
-        let postImg;
-        if (parsedContent[1].props.children.props?.src) {
-          postImg = parsedContent[1].props.children.props?.src;
-        } else {
-          postImg =
-            parsedContent[1].props.children[0].props.children[2].props[
-              "data-src"
-            ];
-        }
-        const postTitle = post.node.title;
-        const reverse = index % 2 === 0; // true for even index, false for odd index
-        return (
-          <Article
-            key={index}
-            image={postImg}
-            paragraph={postParagraph}
-            title={postTitle}
-            reverse={reverse}
-          />
-        );
-      })}
-    </>
-  );
-};
-export default HomePosts;
->>>>>>> walid
